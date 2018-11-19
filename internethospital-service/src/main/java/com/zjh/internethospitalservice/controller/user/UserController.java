2e21b533e917822631b9cdceda5c4158fc46a729
张江浩 <zhangjianghao@zhuojianchina.com>
2018-11-19 17:26:27 +0800

    1.sql语句。

---
+++ internethospital-service/src/main/java/com/zjh/internethospitalservice/controller/user/UserController.java
@@ -0,0 +1,16 @@
+package com.zjh.internethospitalservice.controller.user;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 用户类
+ *
+ * @version 1.00
+ * @author: 张江浩
+ * @date: 2018/11/9 15:11
+ */
+@RestController
+public class UserController {
+
+}
+
