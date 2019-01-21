594d531c74dfcbc4aa3d8d0d6402333c859b8497
张江浩 <zhangjianghao@zhuojianchina.com>
2019-01-21 09:57:54 +0800

    1.资料迁移。

--- internethospital-api/src/main/java/com/zjh/internethospitalapi/service/management/ManagementDoctorService.java
+++ internethospital-api/src/main/java/com/zjh/internethospitalapi/service/management/ManagementDoctorService.java
@@ -8,6 +8,17 @@ package com.zjh.internethospitalapi.service.management;
  * @date: 2019/1/7 15:53
  */
 public interface ManagementDoctorService {
-
+    /**
+     * 新增医生
+     * @param doctorName 医生名字
+     * @param doctorNumber 医生工号
+     * @param doctorIdCard 医生身份证号
+     * @param phone 医生手机号
+     * @param doctorTitle 医生职称 
+     * @param imgId
+     * @param goodAt
+     */
+    void insertDoctor(String doctorName,String doctorNumber,String doctorIdCard,
+                      String phone,String doctorTitle,String imgId,String goodAt);
 
 }
