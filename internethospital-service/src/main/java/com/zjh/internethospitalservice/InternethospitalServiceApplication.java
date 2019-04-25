7eb308b97320c12d440050cf28afcacbb41610ad
张江浩 <zhangjianghao@zhuojianchina.com>
2019-04-25 14:08:09 +0800

    1.支付导入

--- internethospital-service/src/main/java/com/zjh/internethospitalservice/InternethospitalServiceApplication.java
+++ internethospital-service/src/main/java/com/zjh/internethospitalservice/InternethospitalServiceApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
